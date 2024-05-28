523d7befaeb3a93dbbf1eec6ad1e8ff40c816f08
SlayerOfTheBad <git@mechanical.computer>
2024-05-28 22:28:26 +0200

    Added excitement

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ location="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
 current_commit=$(git rev-parse HEAD)
 build="$location/build.sh"
 
-git checkout bootstrap-step1 \
+git checkout bootstrap-step2 \
     && bash $build \
     && git checkout $current_commit
--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mv message.txt message.prev.txt
-echo "Hello $(cat message.prev.txt)" > message.txt
+echo "$(cat message.prev.txt)!!" > message.txt
 rm message.prev.txt
